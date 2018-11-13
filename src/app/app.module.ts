027e468dfde257795c210790a2c68b48633c79c7
Delia <cuibardelia@yahoo.com>
2018-11-13 21:04:51 +0200

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+
+@NgModule({
+  // Note that AppModule declares both application components, AppComponent and HeroesComponent.
+  declarations: [
+    AppComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
